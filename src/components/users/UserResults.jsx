9af0dadcb3ffd8c54c966699cd7d1fa7e16adb64
Brandon Bryan <bshwag@gmail.com>
2023-03-09 14:26:50 -0500

    Initial commit

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useContext } from 'react';
+import { FaSpinner } from 'react-icons/fa';
+import GithubContext from '../../context/github/GithubContext';
+import UserItem from './UserItem';
+
+function UserResults() {
+	const { users, loading, fetchUsers } = useContext(GithubContext);
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	if (!loading) {
+		return (
+			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+				{users.map(user => (
+					<UserItem key={user.id} user={user} />
+				))}
+			</div>
+		);
+	} else {
+		return <FaSpinner className='text-center mx-auto' style={{ fontSize: '3rem' }} />;
+	}
+}
+
+export default UserResults;
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,33 @@
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
+
+const GithubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+
+export const GithubProvider = ({ children }) => {
+	const initialState = {
+		users: [],
+		loading: true
+	};
+
+	const [state, dispatch] = useReducer(githubReducer, initialState);
+
+	const fetchUsers = async () => {
+		const res = await fetch(`${GITHUB_URL}/users`);
+		const data = await res.json();
+
+		dispatch({
+			type: 'GET_USERS',
+			payload: data
+		});
+	};
+
+	return (
+		<GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>
+			{children}
+		</GithubContext.Provider>
+	);
+};
+
+export default GithubContext;
