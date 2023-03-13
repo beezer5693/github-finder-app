f940679d2e5869ff0da8a2fd9eaa4e07fd592b23
Brandon Bryan <bshwag@gmail.com>
2023-03-13 12:27:25 -0400

    Added user profile page and data

---
+++ src/components/layout/Alert.jsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import AlertContext from '../../context/alert/AlertContext';
+import { FaExclamationCircle } from 'react-icons/fa';
+
+function Alert() {
+	const { alert } = useContext(AlertContext);
+
+	return (
+		alert !== null && (
+			<div className='flex items-start mb-4 space-x-2 items-center'>
+				{alert.type === 'error' && <FaExclamationCircle />}
+				<p className='flex-1 text-base font-semibold leading-7 text-white'>
+					<strong>{alert.msg}</strong>
+				</p>
+			</div>
+		)
+	);
+}
+
+export default Alert;
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from 'react';
+import { createRenderer } from 'react-dom/test-utils';
 import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
@@ -6,11 +7,16 @@ const GithubContext = createContext();
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-	const initialState = { users: [], loading: false };
+	const initialState = {
+		users: [],
+		user: {},
+		repos: [],
+		loading: false
+	};
 
 	const [state, dispatch] = useReducer(githubReducer, initialState);
 
-	// Get seearch results
+	// Get search results
 	const searchUsers = async text => {
 		setLoading();
 
@@ -25,6 +31,39 @@ export const GithubProvider = ({ children }) => {
 		});
 	};
 
+	// Get a single user
+	const getUser = async login => {
+		setLoading();
+
+		const res = await fetch(`${GITHUB_URL}/users/${login}`);
+
+		if (res.status === 404) {
+			window.location = '/notfound';
+		} else {
+			const data = await res.json();
+
+			dispatch({
+				type: 'GET_USER',
+				payload: data
+			});
+		}
+	};
+
+	// Get user repos
+	const getUserRepos = async login => {
+		setLoading();
+
+		const params = new URLSearchParams({ sort: 'created', per_page: 10 });
+
+		const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
+		const data = await res.json();
+
+		dispatch({
+			type: 'GET_REPOS',
+			payload: data
+		});
+	};
+
 	// Clear users
 	const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
 
@@ -36,8 +75,12 @@ export const GithubProvider = ({ children }) => {
 			value={{
 				users: state.users,
 				loading: state.loading,
+				user: state.user,
+				repos: state.repos,
 				searchUsers,
-				clearUsers
+				clearUsers,
+				getUser,
+				getUserRepos
 			}}
 		>
 			{children}
